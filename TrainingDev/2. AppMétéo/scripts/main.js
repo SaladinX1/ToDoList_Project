078435dcd52e347ab09af60db7e944b42e1cf1eb
SaladinX1 <hopefulhorizon01@gmail.com>
2022-04-05 16:27:19 +0200

    Affichage dynamique des heures  pour le projet Meteo

--- TrainingDev/2. AppMétéo/scripts/main.js
+++ TrainingDev/2. AppMétéo/scripts/main.js
@@ -1,11 +1,19 @@
 const CLEFAPI = '2ad94a7a38d1c09957d79b4fa9155a64';
+
 let resultatsAPI;
+
+const temps = document.querySelector('.temps');
+const temperature = document.querySelector('.temperature');
+const localisation = document.querySelector('.localisation');
+const heure = document.querySelectorAll('.heure-nom-prevision');
+const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
+
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
 
-        // console.log(position);
 
+        // console.log(position);
         let long = position.coords.longitude;
         let lat = position.coords.latitude;
         AppelAPI(long, lat);
@@ -19,11 +27,43 @@ function AppelAPI(long, lat) {
 
     // console.log(long, lat);
 
+
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`)
         .then((Response) => {
             return Response.json();
         })
         .then((data) => {
-            console.log(data);
+
+            // console.log(data);
+            resultatsAPI = data;
+
+            temps.innerText = resultatsAPI.current.weather[0].description;
+            temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`;
+            localisation.innerText = resultatsAPI.timezone;
+
+            // let heures par tranche de trois avec leurs températures.
+
+            let heureActuelle = new Date().getHours();
+
+            for (let i = 0; i < heure.length; i++) {
+
+                let heureIncr = heureActuelle + i * 3;
+
+                if (heureIncr > 24) {
+                    heure[i].innerText = `${heureIncr - 24} h`;
+                } else if (heureIncr === 24) {
+                    heure[i].innerText = "00 h"
+                } else {
+                    heure[i].innerText = `${heureIncr} h`;
+                }
+                heure[i].innerText = `${heureIncr} h`
+            }
+
+            // temp pour 3h
+            for (let j = 0; j < tempPourH.length; j++) {
+                tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`;
+            }
+
+
         })
 }
