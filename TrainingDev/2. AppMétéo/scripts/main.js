d27fee4cfbf5e51ced206918a30307a0c4b77697
SaladinX1 <hopefulhorizon01@gmail.com>
2022-04-05 17:52:33 +0200

    Etablissement de l'animation chargement pour le projet Meteo

--- TrainingDev/2. AppMétéo/scripts/main.js
+++ TrainingDev/2. AppMétéo/scripts/main.js
@@ -1,9 +1,8 @@
-import tabJoursEnOrdre from "../scripts/Utilitaire/gestionTemps.js";
+import tabJoursEnOrdre from './Utilitaire/gestionTemps.js';
 
-console.log("DEPUIS MAIN JS : " + tabJoursEnOrdre);
+// console.log("DEPUIS MAIN JS:" + tabJoursEnOrdre);
 
-const CLEFAPI = '2ad94a7a38d1c09957d79b4fa9155a64';
-
+const CLEFAPI = '8e9391f167c17b3253b145b2a036ffd4';
 let resultatsAPI;
 
 const temps = document.querySelector('.temps');
@@ -12,13 +11,12 @@ const localisation = document.querySelector('.localisation');
 const heure = document.querySelectorAll('.heure-nom-prevision');
 const tempPourH = document.querySelectorAll('.heure-prevision-valeur');
 const joursDiv = document.querySelectorAll('.jour-prevision-nom');
-const tempJoursDiv = document.querySelectorAll('.jour-prevision-nom');
-
-
+const tempJoursDiv = document.querySelectorAll('.jour-prevision-temp');
+const imgIcone = document.querySelector('.logo-meteo');
+const chargementContainer = document.querySelector('.overlay-icone-chargement');
 
 if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
-
 
         // console.log(position);
         let long = position.coords.longitude;
@@ -26,29 +24,27 @@ if (navigator.geolocation) {
         AppelAPI(long, lat);
 
     }, () => {
-        alert(`Vous avez refusé la géolocalisation, l'application ne peut pas fonctionner, veuillez l'activer, merci.`)
+        alert(`Vous avez refusé la géolocalisation, l'application ne peur pas fonctionner, veuillez l'activer.!`)
     })
 }
 
 function AppelAPI(long, lat) {
 
-    // console.log(long, lat);
-
-
     fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&exclude=minutely&units=metric&lang=fr&appid=${CLEFAPI}`)
-        .then((Response) => {
-            return Response.json();
+        .then((reponse) => {
+            return reponse.json();
         })
         .then((data) => {
-
             // console.log(data);
+
             resultatsAPI = data;
 
             temps.innerText = resultatsAPI.current.weather[0].description;
-            temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`;
+            temperature.innerText = `${Math.trunc(resultatsAPI.current.temp)}°`
             localisation.innerText = resultatsAPI.timezone;
 
-            // let heures par tranche de trois avec leurs températures.
+
+            // les heures, par tranche de trois, avec leur temperature.
 
             let heureActuelle = new Date().getHours();
 
@@ -63,19 +59,35 @@ function AppelAPI(long, lat) {
                 } else {
                     heure[i].innerText = `${heureIncr} h`;
                 }
-                heure[i].innerText = `${heureIncr} h`
+
             }
 
             // temp pour 3h
             for (let j = 0; j < tempPourH.length; j++) {
-                tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`;
+                tempPourH[j].innerText = `${Math.trunc(resultatsAPI.hourly[j * 3].temp)}°`
             }
 
-            // trois premieres lettres des jours
+
+            // trois premieres lettres des jours 
+
             for (let k = 0; k < tabJoursEnOrdre.length; k++) {
                 joursDiv[k].innerText = tabJoursEnOrdre[k].slice(0, 3);
             }
 
 
+            // Temp par jour
+            for (let m = 0; m < 7; m++) {
+                tempJoursDiv[m].innerText = `${Math.trunc(resultatsAPI.daily[m + 1].temp.day)}°`
+            }
+
+            // Icone dynamique 
+            if (heureActuelle >= 6 && heureActuelle < 21) {
+                imgIcone.src = `ressources/jour/${resultatsAPI.current.weather[0].icon}.svg`
+            } else {
+                imgIcone.src = `ressources/nuit/${resultatsAPI.current.weather[0].icon}.svg`
+            }
+
+            chargementContainer.classList.add('disparition');
         })
+
 }
